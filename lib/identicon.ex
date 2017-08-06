244ef9323f89598c608e95de77766dd6c5511af8
mallory <malloryerik@gmail.com>
2017-08-06 19:42:05 +0900

    image.ex, pattern matching, keyname list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,30 @@
+  ### First three colors of hashed string are RGB color values.
+  # then icon is drawn
+  #  1 2 3 2 1
+  #  4 5 6 5 4
+  #  7 8 9 8 7
+  # et cetera
+  # even is colored, odd is white
+
+  # Struct is a map that can be used to store data, and can have a default value
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
 
-  @doc """
-  Hello world.
+  ## Hash a string from the `main` function 
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
 
-  ## Examples
+    %Identicon.Image{hex: hex}
+  end
 
-      iex> Identicon.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+    [r, g, b]
   end
 end
